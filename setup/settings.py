06cf8aba014416203f12b8f7e7808e3cecc599a3
Marcelo Saldanha <marcelo.sfilho97@gmail.com>
2025-09-18 17:35:49 -0300

    feat rotas do proejto

--- setup/settings.py
+++ setup/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'gallery',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'setup.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')], # direcionar para pasta com páginas HTML
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
